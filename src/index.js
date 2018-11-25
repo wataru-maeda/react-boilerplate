53a0ac398899ffa13d6b6a242d156253be690e87
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-25 13:30:29 -0800

    chore: organize style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import Home from './screens/home/Home';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
