52266283f8ba88cc173edec7233412c9c688154e
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-25 12:53:47 -0800

    chore(init): reset project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
-import { Home, Profile } from './screens';
-import registerServiceWorker from './common/registerServiceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const Rooter = () => (
-    <BrowserRouter>
-        <main>
-            <Route path="/" exact component={Home} />
-            <Route path="/Profile" exact component={Profile} />
-        </main>
-    </BrowserRouter>
-)
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<Rooter />, document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
