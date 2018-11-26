219ec37aa8c2cd34c3ea90b64568dd3879fcc972
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-25 23:28:56 -0800

    chore: setup redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { StyleRoot } from 'radium'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/global.css';
-
-import Home from './screens/home';
+import Router from './router'
 import * as serviceWorker from './utils/serviceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <StyleRoot>
+          <Router />
+        </StyleRoot>
+      </Provider>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
