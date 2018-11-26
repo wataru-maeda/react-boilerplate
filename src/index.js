5c27049074b9ee4b4e0c9a60bb1d8453b653ac98
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-25 23:49:08 -0800

    chore: update readme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import { StyleRoot } from 'radium'
 import { Provider } from 'react-redux'
@@ -7,22 +7,18 @@ import store from './redux/store'
 import Router from './router'
 import * as serviceWorker from './utils/serviceWorker';
 
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <StyleRoot>
-          <Router />
-        </StyleRoot>
-      </Provider>
-    )
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <StyleRoot>
+      <Router />
+    </StyleRoot>
+  </Provider>
+)
 
 render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
